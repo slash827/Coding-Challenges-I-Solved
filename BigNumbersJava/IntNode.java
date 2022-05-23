445b4f92bfbde511b3ee26728bb1a14822a079a9
Gilad Battat <17161818+slash827@users.noreply.github.com>
2022-05-23 10:52:07 +0300

    Add files via upload

---
+++ BigNumbersJava/IntNode.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class IntNode {
+    private IntNode _next;
+    private int _value;
+
+    public IntNode(int val) {
+        _value = val;
+        _next = null;
+    }
+
+    public IntNode(int val, IntNode next) {
+        _next = next;
+        _value = val;
+    }
+
+    public int getValue() {
+        return _value;
+    }
+
+    public void setValue(int v) {
+        _value = v;
+    }
+
+    public IntNode getNext( ) {
+        return _next;
+    }
+
+    public void setNext(IntNode node) {
+        _next = node;
+    }
+
+}
