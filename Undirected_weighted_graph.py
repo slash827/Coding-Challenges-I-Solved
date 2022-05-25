e03440090086b6673433b12b73bc801eee2058a5
Gilad Battat <17161818+slash827@users.noreply.github.com>
2022-05-25 23:18:26 +0300

    Add files via upload

--- Undirected_weighted_graph.py
+++ Undirected_weighted_graph.py
@@ -1,26 +1,27 @@
 import operator
 
 
-class Graph():
+class Graph:
 
     def __init__(self, vertices_num):
         # number of nodes (an integer)
         self.v = vertices_num
         # (maybe not useful here) : list of nodes from "A0", "A1" ... to "A index (vertices_num - 1)"
-        self.nodes = ['A' + str(x-1) for x in range(self.v)]
+        self.nodes = ['A' + str(x - 1) for x in range(self.v)]
         self.paths = []
 
     # from adjacency matrix to dictionary
-    def adjmat_2_graph(self, adjm:list):
+    def adjmat_2_graph(self, adjm: list):
         graph = {}
         if type(adjm) != list or adjm is None:
             return None
+
         for i in range(len(adjm)):
             char = 'A' + str(i)
             ls = []
-            for j in range(len(adjm[i])):
-                if adjm[i][j] > 0:
-                    tp = ('A' + str(j), adjm[i][j])
+            for index, adj_lst in enumerate(adjm[i]):
+                if adj_lst > 0:
+                    tp = ('A' + str(index), adj_lst)
                     ls.append(tp)
             graph[char] = ls
         return graph
@@ -31,9 +32,11 @@ class Graph():
         adjm = []
         keys = list(graph.keys())
         n = len(keys)  # the amount of nodes in G
+
         for i in range(n):
             ls = [0 for x in range(n)]
             adjm.append(ls)
+
         for i in range(len(keys)):
             index = int(keys[i][1])
             tup_list = graph[keys[i]]
@@ -50,6 +53,7 @@ class Graph():
         lst = []
         if type(graph) != dict or graph is None:
             return None
+
         keys = list(graph.keys())
         for i in range(len(keys)):
             item = [keys[i], graph[keys[i]]]
@@ -81,6 +85,7 @@ class Graph():
         V = list(graph.keys())
         V.remove(start_vertex)
         layers = [[start_vertex]]
+
         i = 0
         # layers is the list of all layers based on BFS algorithm
         while len(V) > 0:
@@ -101,7 +106,7 @@ class Graph():
                 return i
         return -1
 
-    def opt(self, graph:dict, start_v:str, end_v:str, vertex:str, path_list: str):
+    def opt(self, graph: dict, start_v: str, end_v: str, vertex: str, path_list: str):
         if vertex in path_list:
             return None
         if end_v == vertex:
@@ -109,17 +114,19 @@ class Graph():
             if path_list not in self.paths:
                 self.paths.append(path_list)
             return None
+
         if start_v == vertex and path_list == '':
             path_list = vertex
         else:
             path_list += '-' + vertex
+
         # now we should call all of vertex neighbors with opr
         neighbors = [x[0] for x in graph[vertex]]
         for nei in neighbors:
             self.opt(graph, start_v, end_v, nei, path_list)
 
     # find all path from node start_vertex to node end_vertex
-    def find_all_paths(self, graph:dict, start_vertex:str, end_vertex:str):
+    def find_all_paths(self, graph: dict, start_vertex: str, end_vertex: str):
         # graph is a dictionary
         print(graph)
         print(f'first is: {start_vertex} and last is: {end_vertex}')
@@ -128,6 +135,7 @@ class Graph():
         if self.is_in_layer(first_bfs, end_vertex) == -1:
             print("first")
             return None
+
         V = list(graph.keys())
         if end_vertex not in V or start_vertex not in V:
             print("second")
@@ -145,10 +153,12 @@ class Graph():
 
         return sorted(sorted(self.paths, key=str), key=len)
 
+
 def main():
     G = Graph(6)
-    graph = {'A5': [('A3', 1)], 'A3': [('A0', 1), ('A2', 1)], 'A0': [('A3', 1), ('A5', 1)], 'A4': [('A2', 1)], 'A1': [('A2', 1)], 'A2': [('A1', 1), ('A2', 1), ('A3', 1), ('A4', 1)]}
-    print(G.find_all_paths(graph,'A0','A2'))
+    graph = {'A5': [('A3', 1)], 'A3': [('A0', 1), ('A2', 1)], 'A0': [('A3', 1), ('A5', 1)], 'A4': [('A2', 1)],
+             'A1': [('A2', 1)], 'A2': [('A1', 1), ('A2', 1), ('A3', 1), ('A4', 1)]}
+    print(G.find_all_paths(graph, 'A0', 'A2'))
 
 
 if __name__ == '__main__':
